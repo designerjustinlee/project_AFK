310940d0d9a0adb2d37eff5507b46f726b34cb7b
john <support@rendercore.com>
2021-05-07 12:49:17 +0900

    app컴포넌트에 적용하기

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,10 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { LayoutModule } from './layout/layout.module';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, AppRoutingModule, LayoutModule],
+  imports: [BrowserModule, AppRoutingModule],
   providers: [],
   bootstrap: [AppComponent],
 })
