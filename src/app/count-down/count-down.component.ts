c37a2d4783f8d4db3928f6ddf187001ed4fcb9fe
brian_imac <teamsmiley@gmail.com>
2021-05-07 15:24:09 -0700

    카운트 다운이 항상 보여도 될듯

--- src/app/count-down/count-down.component.ts
+++ src/app/count-down/count-down.component.ts
@@ -11,11 +11,11 @@ import { Output, EventEmitter } from '@angular/core';
 export class CountDownComponent implements OnInit, OnDestroy {
   @Output() doneEvent = new EventEmitter<boolean>();
 
-  isStart: boolean = false;
+  // isStart: boolean = false;
 
   private subscription = new Subscription();
 
-  isDone: boolean | undefined = false;
+  isDone: boolean = false;
 
   constructor() {}
 
@@ -24,8 +24,9 @@ export class CountDownComponent implements OnInit, OnDestroy {
   secondsInAMinute = 60;
 
   timeDifference: number = 0;
+
+  minutesToDday: number = 3;
   secondsToDday: number = 0;
-  minutesToDday: number = 0;
 
   private getTimeDifference(dDay: number) {
     this.timeDifference = dDay - new Date().getTime();
@@ -33,7 +34,7 @@ export class CountDownComponent implements OnInit, OnDestroy {
     this.allocateTimeUnits(this.timeDifference);
 
     if (this.timeDifference < 0) {
-      this.isStart = false;
+      // this.isStart = false;
       this.isDone = true;
       this.subscription.unsubscribe();
       this.doneEvent.emit(true);
@@ -51,10 +52,10 @@ export class CountDownComponent implements OnInit, OnDestroy {
   }
 
   start() {
-    this.isStart = true;
+    // this.isStart = true;
 
     const dDay: Date = new Date();
-    dDay.setSeconds(dDay.getSeconds() + 20);
+    dDay.setSeconds(dDay.getSeconds() + 180);
 
     this.subscription = interval(500).subscribe((x) => {
       this.getTimeDifference(dDay.getTime());
@@ -62,7 +63,7 @@ export class CountDownComponent implements OnInit, OnDestroy {
   }
 
   restart() {
-    this.isStart = false;
+    // this.isStart = false;
     this.isDone = false;
     this.doneEvent.emit(false);
   }
