62030b89b65e0304f320cc82796137aff32ccf08
brian_imac <teamsmiley@gmail.com>
2021-05-07 00:18:34 -0700

    add component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,9 +3,10 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CountDownComponent } from './count-down/count-down.component';
 
 @NgModule({
-  declarations: [AppComponent],
+  declarations: [AppComponent, CountDownComponent],
   imports: [BrowserModule, AppRoutingModule],
   providers: [],
   bootstrap: [AppComponent],
